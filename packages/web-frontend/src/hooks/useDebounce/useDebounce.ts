f1e6887c8ea088eece7ef5af8c4de709f7d3b630
Caiden Sanders <work.caidensanders@gmail.com>
2021-02-07 08:11:20 -0800

    Added useDebounce.ts hook to website frontend

---
+++ packages/web-frontend/src/hooks/useDebounce/useDebounce.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) Caiden Sanders and his affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// React Imports
+import { useState, useEffect } from 'react';
+
+/**
+ * useDebounce Hook
+ *
+ * usage:
+ *      const debouncedValue = useDebounce<type>(value, delay);
+ */
+const useDebounce = <T>(value: T, delay?: number): T => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
