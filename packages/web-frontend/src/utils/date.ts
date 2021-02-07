0374dcfb464be2e88cd907ecee2cb17f46870d90
Caiden Sanders <work.caidensanders@gmail.com>
2021-02-07 08:37:39 -0800

    Added date.ts utility to website frontend

---
+++ packages/web-frontend/src/utils/date.ts
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) Caiden Sanders and his affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Converts unix timestamp into a time ago string, ex: 2 hours ago
+ *
+ * @param {string} date unix timestamp
+ */
+export const timeAgo = (unixTimestamp: string) => {
+  const date = new Date(parseInt(unixTimestamp));
+
+  const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+  let interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) return interval + ' years';
+
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) return interval + ' months';
+
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) return interval + ' days';
+
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) return interval + ' hours';
+
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) return interval + ' mins';
+
+  return Math.floor(seconds) + ' seconds';
+};
+
+/**
+ * Converts unix timestamp to current date
+ *
+ * @param {string} date unix timestamp
+ */
+export const currentDate = (unixTimestamp: string) => {
+  const date = new Date(parseInt(unixTimestamp));
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const month = months[date.getMonth() + 1];
+  const day = date.getDay();
+  const year = date.getFullYear();
+  const time = date.toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  });
+
+  return `${month} ${day}, ${year} ${time}`;
+};
